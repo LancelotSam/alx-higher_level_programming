e3b068379f568a23d2064493cb3026f2cda37f72
LancelotSam <skmburu110@gmail.com>
2023-10-05 02:42:32 +0000

    fix for pycode

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 for number in range(100):
     if number < 10:
-        print("{:02}".format(number)
+        print("{:02}".format(number), end=", ")
     else:
-        print("{}".format(number)", end=", ")
+        print("{}".format(number), end=", ")
+print()
