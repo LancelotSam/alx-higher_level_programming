7b641893d334d8d82728aa907d0a2e9fff2b9f22
LancelotSam <skmburu110@gmail.com>
2023-10-05 00:56:51 +0000

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """ Fizz for multiples of 3
+    Buzz for multiples of 5
+    FizzBuzz for multiples of both"""
+    for number in range(101):
+        if (number % 3 == 0 and number % 5 == 0):
+            print("FizzBuzz", end=' ')
+        elif (number % 3 == 0):
+            print("Fizz", end=' ')
+        elif (number % 5 == 0):
+            print("Buzz", end= ' ')
+        else:
+            print(number, end=' ')
