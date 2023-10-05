03b7bcaaa3023da09374131f96eb9f0889b69f8d
LancelotSam <skmburu110@gmail.com>
2023-10-05 02:48:19 +0000

    fix for pycode

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ def comb(L):
     for digit in range(10):
         for digit1 in range(digit+1,10):
             if (digit!=digit1):
-                print(f"{digit}{digit1}", end=', ')
+                print("{}{}".format"{digit}{digit1}", end=', ')
 comb(range(10))
 print()
