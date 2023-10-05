7a1ffa6c58c6e5b0c536590d50b016b3a7d8e6ec
LancelotSam <skmburu110@gmail.com>
2023-10-05 02:22:43 +0000

    fix for pycode

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-#print alphabet lower ascii vales, not followed by \n
-for alphabet in range(97,123):
+for alphabet in range(97, 123):
     print("{}".format(chr(alphabet)), end="")
