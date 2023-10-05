d27be16d81bbd6c19facbbadb8dff39e0b55e8d6
LancelotSam <skmburu110@gmail.com>
2023-10-05 01:49:18 +0000

    fizzbuzz, corrected for 0

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     """ Fizz for multiples of 3
     Buzz for multiples of 5
     FizzBuzz for multiples of both"""
-    for number in range(101):
+    for number in range(1, 101):
         if (number % 3 == 0 and number % 5 == 0):
             print("FizzBuzz", end=' ')
         elif (number % 3 == 0):
