bd0234de63e7151df7b0e0b9fa9188a136ff176a
LancelotSam <skmburu110@gmail.com>
2023-10-25 22:07:23 +0000

    Add aprivate attribute to the class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """attributes of the class"""
+    def __init__(self, size):
+        """ Class initialization
+        Args:
+           size(int): size of the square
+           """
+        self.__size = size
