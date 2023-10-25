de02b1ca8b328164ca150b7282d696fbe1fa4a25
LancelotSam <skmburu110@gmail.com>
2023-10-25 22:18:54 +0000

    attributes of size using exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining class Square"""
+
+
+class Square():
+    def __init__(self, size=0):
+        """ Initialize the class
+        Args:
+            size(int): size of the square
+            """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
