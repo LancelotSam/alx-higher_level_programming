6b35ab14a4a36d1ddde410b1c3b7b8c567f0de47
Lancelot <skmburu110@gmail.com>
2024-01-09 18:38:58 -0800

    fixed fo semistandard

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      this.width = 0;
+      this.height = 0;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width === 0 || this.height === 0) {
+      console.log('');
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+};
